813d9ac457f4dbdd70017a7f19007939d5c35d79
Flashk <mevengar@gmail.com>
2021-04-02 11:11:27 +0200

    Initial commit

---
+++ src/main/java/brv/telegram/bots/WowMountTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package brv.telegram.bots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WowMountTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WowMountTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/brv/telegram/bots/WowMountTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package brv.telegram.bots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WowMountTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
